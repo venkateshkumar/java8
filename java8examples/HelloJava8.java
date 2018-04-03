c8391e93a54cdbf9106d9a06d90bc87d469650d8
unknown <venkateshkumarsr@gmail.com>
2018-04-03 22:54:12 +0400

    Comitted the first files on Java 8

---
+++ java8examples/HelloJava8.java
@@ -0,0 +1,24 @@
+package com.java8examples;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 
+ * @author Venkateshkumar.S
+ * 
+ * Demonstrates sample functions in java 8
+ *
+ */
+public class HelloJava8 {
+	public static void main(String[] args) {
+		List<String> lstNames =new ArrayList<>();
+		lstNames.add("Sai");
+		lstNames.add("Ram");
+		lstNames.add("Sai");
+		System.out.println(lstNames.parallelStream().count());	
+		List<String> distinctNames =lstNames.stream().distinct().collect(Collectors.toList());
+		System.out.println(distinctNames.size());
+	}
+}
