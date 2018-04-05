4204778c8090bfbbd4b9223ce086855d27413d91
Sai.JananeVenkatesh <venkateshkumarsr@gmail.com>
2018-04-05 23:18:18 +0400

    App

---
+++ ang-app/src/main/java/com/akms/AngAppApplication.java
@@ -0,0 +1,12 @@
+package com.akms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngAppApplication.class, args);
+	}
+}
